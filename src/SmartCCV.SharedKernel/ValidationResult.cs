dcb3a95e39944303ef4a7aec74b941bfede81c91
Khaledov <khaled.m.ramadan@gmail.com>
2022-07-06 22:52:15 +0200

    adding sharedkernel lib

---
+++ src/SmartCCV.SharedKernel/ValidationResult.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace SmartCCV.SharedKernel
+{
+    public class ValidationResult
+    {
+        public bool IsValid { get; set; }
+        public List<string> Errors { get; set; }
+        public ValidationResult()
+        {
+            IsValid = true;
+            Errors = new List<string>();
+        }
+
+        public void AddError(string errorMessage)
+        {
+            Errors.Add(errorMessage);
+        }
+    }
+}
